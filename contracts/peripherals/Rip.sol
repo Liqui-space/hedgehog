7906a3156a29369f0f8d9d1b9d10ff0a5b1d6169
ivan <vivan.volovik@gmail.com>
2024-01-08 12:12:52 +0200

    renaming

--- contracts/peripherals/Rip.sol
+++ contracts/peripherals/Rip.sol
@@ -4,75 +4,76 @@ pragma solidity =0.8.4;
 import "hardhat/console.sol";
 
 contract Rip {
-    struct Delegate {
+    struct Owner {
         bool exists;
         uint256 activationBlock;
     }
 
-    mapping(address => Delegate) public delegates;
+    mapping(address => Owner) public owners;
+    mapping(address => bool) public isOwner;
 
     address public multisig;
 
     uint256 public timelockInBlocks = 4 * 60 * 24 * 7 * 4 * 3; // 3 months
 
-    constructor(address _multisig, address delegate1, address delegate2) {
+    constructor(address _multisig, address owner1, address owner2) {
         multisig = _multisig;
-        _addDelegate(delegate1);
-        _addDelegate(delegate2);
+        _addOwner(owner1);
+        _addOwner(owner2);
     }
 
     // check is 2 out of 3 is enougth to remove this contract from multisig
     function executeCall(
         address target,
         bytes memory data
-    ) public payable isDelegate isActivatedDelegate returns (bytes memory) {
+    ) public payable isOwner isActivatedOwner returns (bytes memory) {
         (bool success, bytes memory response) = target.call{value: msg.value}(data);
         require(success, "External call failed");
         return response;
     }
 
-    //? delegate activation
+    //? owner activation
 
-    function activateDelegate() external isDelegate {
-        delegates[msg.sender].activationBlock = block.number;
+    function activateOwner() external isOwner {
+        owners[msg.sender].activationBlock = block.number;
     }
 
-    function deactivateDelegate(address _delegate) external isDelegate {
-        delegates[_delegate].activationBlock = 0;
+    function deactivateOwner(address _owner) external isOwner {
+        owners[_owner].activationBlock = 0;
     }
 
     function setTimelockInBlocks(uint256 _timelockInBlocks) external isAdmin {
         timelockInBlocks = _timelockInBlocks;
     }
 
-    function addDelegate(address delegate) external isAdmin {
-        _addDelegate(delegate);
+    function addOwner(address owner) external isAdmin {
+        _addOwner(owner);
     }
 
-    function removeDelegate(address delegate) external isAdmin {
-        delete delegates[delegate];
+    function removeOwner(address owner) external isAdmin {
+        delete owners[owner];
     }
 
     //? internal
 
-    function _addDelegate(address delegate) internal {
-        require(!delegates[delegate].exists, "Delegate already exists");
-        delegates[delegate] = Delegate(true, 0);
+    function _addOwner(address owner) internal {
+        require(!owners[owner].exists, "Owner already exists");
+        owners[owner] = Owner(true, 0);
     }
 
     //? modifiers
 
-    modifier isActivatedDelegate() {
+    modifier isActivatedOwner() {
         require(
-            delegates[msg.sender].activationBlock > 0 &&
-                block.number - delegates[msg.sender].activationBlock >= timelockInBlocks,
+            owners[msg.sender].activationBlock > 0 &&
+                block.number - owners[msg.sender].activationBlock >= timelockInBlocks,
             "Not activated"
         );
         _;
     }
 
-    modifier isDelegate() {
-        require(delegates[msg.sender].exists, "Not a delegate");
+    modifier isOwner() {
+        require(owners[msg.sender].exists, "Not a owner");
         _;
     }
 
--- test/production/9 - rip.test.js
+++ test/production/9 - rip.test.js
@@ -8,7 +8,7 @@ const { shouldThrowErrorComponent, executeTx } = require("../helpers/components"
 const { assert } = require("chai");
 const { utils } = require("ethers");
 
-describe.only("Rip test mainnet", function () {
+describe("Rip test mainnet", function () {
     let Rip;
     it("Initial", async function () {
         await resetFork(18942139);
@@ -21,39 +21,39 @@ describe.only("Rip test mainnet", function () {
         assert((await getERC20Balance(Rip, wethAddress)) == utils.parseEther("1"), "Should be 1");
     });
 
-    it("Could add delegate", async function () {
+    it("Could add owner", async function () {
         await shouldThrowErrorComponent(
-            Rip.connect(deployer).addDelegate(admin3.address),
+            Rip.connect(deployer).addOwner(admin3.address),
             "VM Exception while processing transaction: reverted with reason string 'Not multisig'",
             "Should be error"
         );
         await shouldThrowErrorComponent(
-            Rip.connect(chad).addDelegate(admin3.address),
+            Rip.connect(chad).addOwner(admin3.address),
             "VM Exception while processing transaction: reverted with reason string 'Not multisig'",
             "Should be error"
         );
         await shouldThrowErrorComponent(
-            Rip.connect(admin1).addDelegate(admin3.address),
+            Rip.connect(admin1).addOwner(admin3.address),
             "VM Exception while processing transaction: reverted with reason string 'Not multisig'",
             "Should be error"
         );
         await shouldThrowErrorComponent(
-            Rip.connect(multisig).addDelegate(admin1.address),
-            "VM Exception while processing transaction: reverted with reason string 'Delegate already exists'",
+            Rip.connect(multisig).addOwner(admin1.address),
+            "VM Exception while processing transaction: reverted with reason string 'Owner already exists'",
             "Should be error"
         );
 
-        await executeTx(Rip.connect(multisig).addDelegate(admin3.address));
+        await executeTx(Rip.connect(multisig).addOwner(admin3.address));
     });
 
-    it("Could activate delegate", async function () {
-        assert((await Rip.delegates(admin3.address)).activationBlock == 0, "Should be 0");
+    it("Could activate owner", async function () {
+        assert((await Rip.owners(admin3.address)).activationBlock == 0, "Should be 0");
 
-        await executeTx(Rip.connect(admin3).activateDelegate(), "delegate activation");
+        await executeTx(Rip.connect(admin3).activateOwner(), "owner activation");
 
         await shouldThrowErrorComponent(
             Rip.connect(multisig).executeCall(wethAddress, data),
-            "VM Exception while processing transaction: reverted with reason string 'Not a delegate'",
+            "VM Exception while processing transaction: reverted with reason string 'Not a owner'",
             "Should be error"
         );
         await shouldThrowErrorComponent(
@@ -67,8 +67,8 @@ describe.only("Rip test mainnet", function () {
         await executeTx(Rip.connect(admin3).executeCall(wethAddress, data), "succesful call");
     });
 
-    it("Could deactivate delegate", async function () {
-        await executeTx(Rip.connect(admin2).deactivateDelegate(admin3.address), "deactivate delegate");
+    it("Could deactivate owner", async function () {
+        await executeTx(Rip.connect(admin2).deactivateOwner(admin3.address), "deactivate owner");
 
         await shouldThrowErrorComponent(
             Rip.connect(admin3).executeCall(wethAddress, data),
@@ -87,7 +87,7 @@ describe.only("Rip test mainnet", function () {
         const timeLockInBlocks = (await Rip.timelockInBlocks()).toNumber();
         assert(timeLockInBlocks == 10, "Should be 10");
 
-        await executeTx(Rip.connect(admin2).activateDelegate(), "delegate activation");
+        await executeTx(Rip.connect(admin2).activateOwner(), "owner activation");
 
         await shouldThrowErrorComponent(
             Rip.connect(admin2).executeCall(wethAddress, data),
@@ -100,18 +100,18 @@ describe.only("Rip test mainnet", function () {
         await executeTx(Rip.connect(admin2).executeCall(wethAddress, data), "succesful call");
     });
 
-    it("Could remove delegate", async function () {
+    it("Could remove owner", async function () {
         await shouldThrowErrorComponent(
-            Rip.connect(admin3).removeDelegate(admin2.address),
+            Rip.connect(admin3).removeOwner(admin2.address),
             "VM Exception while processing transaction: reverted with reason string 'Not multisig'",
             "Should be error"
         );
 
-        await executeTx(Rip.connect(multisig).removeDelegate(admin2.address), "remove delegate");
+        await executeTx(Rip.connect(multisig).removeOwner(admin2.address), "remove owner");
 
         await shouldThrowErrorComponent(
             Rip.connect(admin2).executeCall(wethAddress, data),
-            "VM Exception while processing transaction: reverted with reason string 'Not a delegate'",
+            "VM Exception while processing transaction: reverted with reason string 'Not a owner'",
             "Should be error"
         );
 
